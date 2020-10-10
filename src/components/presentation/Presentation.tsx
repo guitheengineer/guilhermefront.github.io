5fdaf2ea6231eef2b6abbb149a8ef9773779e30c
gsdeveloper <gsdevelopercontact@gmail.com>
2020-10-10 19:41:16 -0300

    feat: add profile component

--- src/components/presentation/Presentation.tsx
+++ src/components/presentation/Presentation.tsx
@@ -1,18 +1,22 @@
 import { BlueButton } from 'components/blue-button';
+import { Profile } from 'components/profile';
 import React from 'react';
 import './presentation.scss';
 
 const Presentation = ({}) => {
   return (
-    <section className="Presentation">
-      <h1 className="Presentation__heading">&#128075; Hi, i'm Guilherme</h1>
-      <p className="Presentation__paragraph">
-        As a React developer, I've been coding valuable applications with
-        continuous improvements. My goal is to always improve people daily lives
-        by providing quality software
-      </p>
-      <BlueButton className="Presentation__button">Contact</BlueButton>
-    </section>
+    <div className="Presentation">
+      <section className="Presentation__call">
+        <h1 className="Presentation__heading">&#128075; Hi, i'm Guilherme</h1>
+        <p className="Presentation__paragraph">
+          As a React developer, I've been coding valuable applications with
+          continuous improvements. My goal is to always improve people daily
+          lives by providing quality software
+        </p>
+        <BlueButton className="Presentation__button">Contact</BlueButton>
+      </section>
+      <Profile />
+    </div>
   );
 };
 export default Presentation;
