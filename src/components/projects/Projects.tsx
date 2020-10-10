d326733e24a55d6ea8630d5b7f0ce4be10891c59
gsdeveloper <gsdevelopercontact@gmail.com>
2020-10-10 19:42:06 -0300

    feat: refactor projects to receive styling

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -13,74 +13,76 @@ const Projects = ({}) => {
   return (
     <article className="Projects">
       <h6 className="Projects__heading">Projects</h6>
-      <Work>
-        <WorkInfo>
-          <WorkCompany>Personal project</WorkCompany>
-          <WorkTitle>Quizby</WorkTitle>
-          <WorkRoles>Fullstack developer, UI/UX designer</WorkRoles>
-          <WorkStack>
-            <WorkIcon title="react" />
-            <WorkIcon title="redux" />
-            <WorkIcon title="figma" />
-            <WorkIcon title="mongo" />
-            <WorkIcon title="sass" />
-            <WorkIcon title="node" />
-            <WorkIcon title="typescript" />
-          </WorkStack>
-        </WorkInfo>
-        <WorkImage image="first" />
-      </Work>
-      <Work>
-        <WorkInfo>
-          <WorkCompany>Personal project</WorkCompany>
-          <WorkTitle>BetterAim</WorkTitle>
-          <WorkRoles>Front-end developer, UI/UX designer</WorkRoles>
-          <WorkStack>
-            <WorkIcon title="react" />
-            <WorkIcon title="figma" />
-            <WorkIcon title="sass" />
-            <WorkIcon title="typescript" />
-          </WorkStack>
-        </WorkInfo>
-        <WorkImage image="first" />
-      </Work>
-      <Work>
-        <WorkInfo>
-          <WorkCompany>MICG</WorkCompany>
-          <WorkTitle>Budget estimator</WorkTitle>
-          <WorkRoles>React Developer, UI/UX designer</WorkRoles>
-          <WorkStack>
-            <WorkIcon title="react" />
-            <WorkIcon title="figma" />
-            <WorkIcon title="styled-components" />
-          </WorkStack>
-        </WorkInfo>
-        <WorkImage image="first" />
-      </Work>
-      <Work>
-        <WorkInfo>
-          <WorkCompany>Personal project</WorkCompany>
-          <WorkTitle>Twitter Trending</WorkTitle>
-          <WorkRoles>Front-end developer, UI/UX designer</WorkRoles>
-          <WorkStack>
-            <WorkIcon title="react" />
-            <WorkIcon title="figma" />
-          </WorkStack>
-        </WorkInfo>
-        <WorkImage image="first" />
-      </Work>
-      <Work>
-        <WorkInfo>
-          <WorkCompany>Personal project</WorkCompany>
-          <WorkTitle>Donut</WorkTitle>
-          <WorkRoles>React Developer</WorkRoles>
-          <WorkStack>
-            <WorkIcon title="react" />
-            <WorkIcon title="typescript" />
-          </WorkStack>
-        </WorkInfo>
-        <WorkImage image="first" />
-      </Work>
+      <div className="Projects__works">
+        <Work>
+          <WorkInfo>
+            <WorkCompany>Personal project</WorkCompany>
+            <WorkTitle>Quizby</WorkTitle>
+            <WorkRoles>Fullstack developer, UI/UX designer</WorkRoles>
+            <WorkStack>
+              <WorkIcon title="react" />
+              <WorkIcon title="redux" />
+              <WorkIcon title="figma" />
+              <WorkIcon title="mongo" />
+              <WorkIcon title="sass" />
+              <WorkIcon title="node" />
+              <WorkIcon title="typescript" />
+            </WorkStack>
+          </WorkInfo>
+          <WorkImage image="first" />
+        </Work>
+        <Work>
+          <WorkInfo>
+            <WorkCompany>Personal project</WorkCompany>
+            <WorkTitle>BetterAim</WorkTitle>
+            <WorkRoles>Front-end developer, UI/UX designer</WorkRoles>
+            <WorkStack>
+              <WorkIcon title="react" />
+              <WorkIcon title="figma" />
+              <WorkIcon title="sass" />
+              <WorkIcon title="typescript" />
+            </WorkStack>
+          </WorkInfo>
+          <WorkImage image="first" />
+        </Work>
+        <Work>
+          <WorkInfo>
+            <WorkCompany>MICG</WorkCompany>
+            <WorkTitle>Budget estimator</WorkTitle>
+            <WorkRoles>React Developer, UI/UX designer</WorkRoles>
+            <WorkStack>
+              <WorkIcon title="react" />
+              <WorkIcon title="figma" />
+              <WorkIcon title="styled-components" />
+            </WorkStack>
+          </WorkInfo>
+          <WorkImage image="first" />
+        </Work>
+        <Work>
+          <WorkInfo>
+            <WorkCompany>Personal project</WorkCompany>
+            <WorkTitle>Twitter Trending</WorkTitle>
+            <WorkRoles>Front-end developer, UI/UX designer</WorkRoles>
+            <WorkStack>
+              <WorkIcon title="react" />
+              <WorkIcon title="figma" />
+            </WorkStack>
+          </WorkInfo>
+          <WorkImage image="first" />
+        </Work>
+        <Work>
+          <WorkInfo>
+            <WorkCompany>Personal project</WorkCompany>
+            <WorkTitle>Donut</WorkTitle>
+            <WorkRoles>React Developer</WorkRoles>
+            <WorkStack>
+              <WorkIcon title="react" />
+              <WorkIcon title="typescript" />
+            </WorkStack>
+          </WorkInfo>
+          <WorkImage image="first" />
+        </Work>
+      </div>
     </article>
   );
 };
