dc58865d6ee9ca7ba172ca21bc73a58063adb5e1
gsdeveloper <gsdevelopercontact@gmail.com>
2020-10-10 13:51:14 -0300

    feat: add simple nav component

---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,18 @@
+import { PersonalIcon } from 'components/personal-icon';
+import React from 'react';
+import './nav.scss';
+
+const Nav = () => (
+  <nav className="Nav">
+    <h4 className="Nav__title">Guilherme Samuel</h4>
+    <ul className="Nav__list">
+      <PersonalIcon className="Nav__item Nav__item--github" title="github" />
+      <PersonalIcon
+        className="Nav__item Nav__item--linkedin"
+        title="linkedin"
+      />
+    </ul>
+  </nav>
+);
+
+export default Nav;
