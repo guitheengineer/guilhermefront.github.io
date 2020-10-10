d21712134b49b6e332841f217ef539a030f9bddc
gsdeveloper <gsdevelopercontact@gmail.com>
2020-10-10 16:06:58 -0300

    feat: add social links and cursor pointer

--- src/components/personal-icon/PersonalIcon.tsx
+++ src/components/personal-icon/PersonalIcon.tsx
@@ -2,17 +2,25 @@ import React from 'react';
 import './personal-icon.scss';
 
 type Props = {
-  title: string;
+  title: 'github' | 'linkedin';
   className?: string;
   size?: number;
 };
 
 const PersonalIcon = ({ title, className, size = 19 }: Props) => (
-  <img
-    className={className}
-    style={{ height: `${size}px` }}
-    src={require(`assets/personal-icons/${title}.png`)}
-  />
+  <a
+    href={`${
+      title === 'github'
+        ? 'https://github.com/gsdeveloper'
+        : 'https://www.linkedin.com/in/guilherme-samuel-2aa7aa19b/'
+    }`}
+  >
+    <img
+      className={`Personal-icon ${className}`}
+      style={{ height: `${size}px` }}
+      src={require(`assets/personal-icons/${title}.png`)}
+    />
+  </a>
 );
 
 export default PersonalIcon;
