086f6b51627ac565447ffa54b55ac255ddf548cf
gsdeveloper <gsdevelopercontact@gmail.com>
2020-10-10 13:44:40 -0300

    feat: add reusable button component

---
+++ src/components/blue-button/BlueButton.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react';
+import './blue-button.scss';
+
+type Props = {
+  children: ReactNode;
+  className: string;
+};
+
+const BlueButton = ({ children, className }: Props) => (
+  <button className={`Blue-button ${className}`}>{children}</button>
+);
+
+export default BlueButton;
