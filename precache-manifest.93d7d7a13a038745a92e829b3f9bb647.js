a1a2b7f11c1955851af5c63a124fa669ec5edfc7
gsdeveloper <gsdevelopercontact@gmail.com>
2020-10-11 01:37:35 -0300

    Updates

---
+++ precache-manifest.93d7d7a13a038745a92e829b3f9bb647.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5587ffa4794e331a14da942d4a7bbb31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "443fddf477d47e44028d",
+    "url": "/static/css/main.17fde78a.chunk.css"
+  },
+  {
+    "revision": "e0620b091aa677de2736",
+    "url": "/static/js/2.828d63bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.828d63bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "443fddf477d47e44028d",
+    "url": "/static/js/main.4459bd61.chunk.js"
+  },
+  {
+    "revision": "d02572eab7c30598406e",
+    "url": "/static/js/runtime-main.1908cb15.js"
+  },
+  {
+    "revision": "7f1e5b5c2f0489d6c74d3d942d3147c2",
+    "url": "/static/media/first.7f1e5b5c.png"
+  },
+  {
+    "revision": "31dc4d54d554be2e798c30e16f02bd5f",
+    "url": "/static/media/profile-picture.31dc4d54.png"
+  }
+]);
