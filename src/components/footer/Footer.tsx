77da0486b977bc03a09bc15c0419f1673cc3d586
gsdeveloper <gsdevelopercontact@gmail.com>
2020-10-12 12:35:03 -0300

    feat: add footer section

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './footer.scss';
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <div className="Footer__content">
+        <p className="Footer__paragraph Footer__paragraph--feedback">
+          Your feedback is appreciated!
+        </p>
+        <p className="Footer__paragraph Footer__paragraph--me">
+          Guilherme Samuel <br /> Front-end developer
+        </p>
+        <p className="Footer__paragraph Footer__paragraph--contact">
+          gsdevelopercontact@gmail.com
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
