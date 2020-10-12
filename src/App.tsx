30a65f004775319ee033eaf825b320da2f501d83
gsdeveloper <gsdevelopercontact@gmail.com>
2020-10-12 12:36:30 -0300

    feat: add aboutme and footer to app

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Projects } from 'components/sections/projects';
 import { Nav } from 'components/nav';
 import { Presentation } from './components/sections/presentation';
 import { Skills } from 'components/sections/skills';
+import { AboutMe } from 'components/sections/about-me';
+import { Footer } from 'components/footer';
 
 const App = () => (
   <div className="App">
@@ -11,6 +13,8 @@ const App = () => (
     <Presentation />
     <Projects />
     <Skills />
+    <AboutMe />
+    <Footer />
   </div>
 );
 
