4a91ae797e272b86ad6a4b98fc536ea5d2beb7ab
gsdeveloper <gsdevelopercontact@gmail.com>
2020-11-05 22:38:45 -0300

    feat: change present emoji to an svg icon for consistency

--- src/components/sections/presentation/Presentation.tsx
+++ src/components/sections/presentation/Presentation.tsx
@@ -3,15 +3,22 @@ import { Profile } from 'components/profile';
 import React from 'react';
 import './presentation.scss';
 
-const Presentation = ({}) => {
+const Presentation = () => {
   return (
     <div className="Presentation">
       <section className="Presentation__call">
-        <h1 className="Presentation__heading">&#128075; Hi, i'm Guilherme</h1>
+        <h1 className="Presentation__heading">
+          <img
+            className="Presentation__present-icon"
+            alt="present-icon"
+            src={require('assets/hands-on.svg')}
+          />
+          Hi, i'm Guilherme
+        </h1>
         <p className="Presentation__paragraph">
           As a React developer, I've been coding valuable applications with
           continuous improvements. My goal is to always improve people daily
-          lives by providing quality software
+          lives by providing quality software.
         </p>
         <a href="mailto:gsdevelopercontact@gmail.com">
           <BlueButton className="Presentation__button">Contact</BlueButton>
