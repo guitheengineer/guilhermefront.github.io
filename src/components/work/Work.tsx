8142d5ff8a7bf66f33dbd567a27b0b1d51c54e59
gsdeveloper <gsdevelopercontact@gmail.com>
2020-11-09 19:40:32 -0300

    feat: add navigate based on project title

--- src/components/work/Work.tsx
+++ src/components/work/Work.tsx
@@ -1,10 +1,17 @@
 import React, { ReactNode } from 'react';
 import './work.scss';
+import { navigate } from '@reach/router';
 
-type Props = { children: ReactNode };
+type Props = { children: ReactNode; title: string };
 
-const Work = ({ children }: Props) => {
-  return <div className="Work">{children}</div>;
-};
+const Work = ({ children, title }: Props) => (
+  <button
+    onClick={() => navigate(`/projects/${title}`)}
+    type="button"
+    className="Work"
+  >
+    {children}
+  </button>
+);
 
 export default Work;
