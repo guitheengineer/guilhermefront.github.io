e862f80ea90b95cf15281a065e2b9f3f36974018
gsdeveloper <gsdevelopercontact@gmail.com>
2020-12-28 19:16:28 -0300

    feat: add responsiveness to content

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode, useEffect, useState } from 'react';
 import './content.scss';
+import { useMediaQuery } from 'react-responsive';
 
 type Props = {
   children: ReactNode;
@@ -18,7 +19,52 @@ export const Content = ({ children, type, className, marginTop }: Props) => {
     requireImage();
   }, []);
 
-  return (
+  const is768 = useMediaQuery({ maxWidth: 768 });
+
+  return is768 ? (
+    <div
+      className="Content"
+      style={{
+        marginTop: marginTop
+          ? marginTop
+          : type === 'story'
+          ? 60
+          : type === 'lessons learned'
+          ? 70
+          : 0,
+      }}
+    >
+      <div className="Content__text">
+        <h1 className="Content__title">
+          {type.charAt(0).toUpperCase() + type.slice(1)}
+        </h1>
+        {type === 'story' ? (
+          <div
+            className={`Content__illustration Content__illustration--story ${className}`}
+          >
+            <img className="Content__img" alt="illustration" src={currentImg} />
+          </div>
+        ) : null}
+        {type === 'lessons learned' ? (
+          <div
+            className={`Content__illustration Content__illustration--lessons-learned ${className}`}
+          >
+            <img className="Content__img" alt="illustration" src={currentImg} />
+          </div>
+        ) : null}
+        {type === 'challenges' ? (
+          <div className={`Content__illustration ${className}`}>
+            <img
+              className="Content__img Content__img--challenges"
+              alt="illustration"
+              src={currentImg}
+            />
+          </div>
+        ) : null}
+        <p className="Content__description">{children}</p>
+      </div>
+    </div>
+  ) : (
     <div
       className="Content"
       style={{
@@ -32,7 +78,9 @@ export const Content = ({ children, type, className, marginTop }: Props) => {
       }}
     >
       {type === 'lessons learned' ? (
-        <div className={`Content__illustration--lessons-learned ${className}`}>
+        <div
+          className={`Content__illustration Content__illustration--lessons-learned ${className}`}
+        >
           <img className="Content__img" alt="illustration" src={currentImg} />
         </div>
       ) : null}
@@ -42,9 +90,20 @@ export const Content = ({ children, type, className, marginTop }: Props) => {
         </h1>
         <p className="Content__description">{children}</p>
       </div>
+      {type === 'story' ? (
+        <div
+          className={`Content__illustration Content__illustration--story ${className}`}
+        >
+          <img className="Content__img" alt="illustration" src={currentImg} />
+        </div>
+      ) : null}
       {type === 'challenges' ? (
         <div className={`Content__illustration ${className}`}>
-          <img className="Content__img" alt="illustration" src={currentImg} />
+          <img
+            className="Content__img Content__img--challenges"
+            alt="illustration"
+            src={currentImg}
+          />
         </div>
       ) : null}
     </div>
