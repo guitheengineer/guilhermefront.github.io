c320d702c0a1c856d4ce281bb393d36d6e19c8ca
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-20 23:55:05 -0300

    feat: automate projects data

--- src/components/sections/projects-group/ProjectsGroup.tsx
+++ src/components/sections/projects-group/ProjectsGroup.tsx
@@ -1,90 +1,25 @@
 import React from 'react';
 import './projects-group.scss';
-import {
-  Work,
-  WorkCompany,
-  WorkImage,
-  WorkInfo,
-  WorkRoles,
-  WorkStack,
-  WorkTitle,
-} from 'components/work';
-import { WorkIcon } from 'components/work-icon';
+import { Work } from 'components/work';
 import { Heading } from 'components/heading';
+import Projects from 'projects';
+
+type Props = {
+  quantity?: number;
+};
+
+export const ProjectList = ({ quantity }: Props) => (
+  <div className="Projects-group__works">
+    {Projects.slice(0, quantity ? quantity : Projects.length).map((project) => (
+      <Work key={project.title} {...project} />
+    ))}
+  </div>
+);
 
 const ProjectsGroup = () => (
   <article className="Projects-group">
     <Heading>Projects</Heading>
-    <div className="Projects-group__works">
-      <Work title="Quizby">
-        <WorkInfo>
-          <WorkCompany>Personal project</WorkCompany>
-          <WorkTitle>Quizby</WorkTitle>
-          <WorkRoles>Fullstack developer, UI/UX designer</WorkRoles>
-          <WorkStack>
-            <WorkIcon title="react" />
-            <WorkIcon title="redux" />
-            <WorkIcon title="figma" />
-            <WorkIcon title="mongo" />
-            <WorkIcon title="sass" />
-            <WorkIcon title="node" />
-            <WorkIcon title="typescript" />
-          </WorkStack>
-        </WorkInfo>
-        <WorkImage image="first" />
-      </Work>
-      <Work title="BetterAim">
-        <WorkInfo>
-          <WorkCompany>Personal project</WorkCompany>
-          <WorkTitle>BetterAim</WorkTitle>
-          <WorkRoles>Front-end developer, UI/UX designer</WorkRoles>
-          <WorkStack>
-            <WorkIcon title="react" />
-            <WorkIcon title="figma" />
-            <WorkIcon title="sass" />
-            <WorkIcon title="typescript" />
-          </WorkStack>
-        </WorkInfo>
-        <WorkImage image="first" />
-      </Work>
-      <Work title="Budget estimator">
-        <WorkInfo>
-          <WorkCompany>MICG</WorkCompany>
-          <WorkTitle>Budget estimator</WorkTitle>
-          <WorkRoles>React Developer, UI/UX designer</WorkRoles>
-          <WorkStack>
-            <WorkIcon title="react" />
-            <WorkIcon title="figma" />
-            <WorkIcon title="styled-components" />
-          </WorkStack>
-        </WorkInfo>
-        <WorkImage image="first" />
-      </Work>
-      <Work title="Budget estimator">
-        <WorkInfo>
-          <WorkCompany>Personal project</WorkCompany>
-          <WorkTitle>Twitter Trending</WorkTitle>
-          <WorkRoles>Front-end developer, UI/UX designer</WorkRoles>
-          <WorkStack>
-            <WorkIcon title="react" />
-            <WorkIcon title="figma" />
-          </WorkStack>
-        </WorkInfo>
-        <WorkImage image="first" />
-      </Work>
-      <Work title="Donut">
-        <WorkInfo>
-          <WorkCompany>Personal project</WorkCompany>
-          <WorkTitle>Donut</WorkTitle>
-          <WorkRoles>React Developer</WorkRoles>
-          <WorkStack>
-            <WorkIcon title="react" />
-            <WorkIcon title="typescript" />
-          </WorkStack>
-        </WorkInfo>
-        <WorkImage image="first" />
-      </Work>
-    </div>
+    <ProjectList />
   </article>
 );
 export default ProjectsGroup;
