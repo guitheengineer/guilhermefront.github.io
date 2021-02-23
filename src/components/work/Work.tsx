856d8b7676f3f5d5164a50ec773ec10b1af40409
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-23 08:44:48 -0300

    refactor: update work to render the data passed in projects.ts

--- src/components/work/Work.tsx
+++ src/components/work/Work.tsx
@@ -1,10 +1,18 @@
-import React, { ReactNode } from 'react';
 import './work.scss';
 import { navigate, useParams } from '@reach/router';
+import { Projects, Companies, Stack } from 'types';
+import WorkImage from './WorkImage';
+import { WorkIcon } from 'components/work-icon';
 
-type Props = { children: ReactNode; title: string };
+type Props = {
+  title: Projects;
+  company: Companies;
+  role: string | string[];
+  image: 'first';
+  stack: Stack;
+};
 
-const Work = ({ children, title }: Props) => {
+const Work = ({ title, company, role, image, stack }: Props) => {
   const { project } = useParams();
 
   return project !== title.toLowerCase() ? (
@@ -13,7 +21,19 @@ const Work = ({ children, title }: Props) => {
       type="button"
       className="Work"
     >
-      {children}
+      <div className="Work__info">
+        <h4 className="Work__company">{company}</h4>
+        <h3 className="Work__title">{title}</h3>
+        <p className="Work__roles">
+          {Array.isArray(role) ? role.reduce((acc, v) => acc + ', ' + v) : role}
+        </p>
+        <ul className="Work__stack">
+          {stack.map((tech, i) => (
+            <WorkIcon key={i} title={tech} />
+          ))}
+        </ul>
+      </div>
+      <WorkImage image={image} />
     </button>
   ) : null;
 };
