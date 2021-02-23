737c43a23638150a1047adc1fd3e3ff161da8884
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-23 14:18:26 -0300

    refactor: abstract see more to pass for every project

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -1,12 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import './project.scss';
 import { RouteComponentProps, useParams } from '@reach/router';
-import { BetterAim } from './project-pages';
 import { ContactArea } from 'components/contact-area';
 import { OtherProjects } from 'components/other-projects';
 import { Footer } from 'components/footer';
+import { useMediaQuery } from 'react-responsive';
+import Projects from 'projects';
+import { ProjectContent } from 'types';
+import { Quizby, BetterAim } from './project-components';
+import { Nav } from 'components/nav';
 
 const components = {
+  quizby: Quizby,
   betteraim: BetterAim,
 };
 
@@ -15,21 +20,38 @@ type Projects = { project: 'betteraim' };
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Project = (_props: RouteComponentProps) => {
   const { project }: Projects = useParams();
-
   const [currentImg, setCurrentImg] = useState('');
+  const [seeMore, setSeeMore] = useState(false);
+
+  const is768 = useMediaQuery({ maxWidth: 768 });
 
   useEffect(() => {
-    const requireImage = async () => {
-      const img = await import(`assets/work-demos/${project}.png`);
-      setCurrentImg(img.default);
-    };
-    requireImage();
+    import(`assets/work-demos/${project}.png`).then((img) =>
+      setCurrentImg(img.default)
+    );
+  }, []);
+
+  const setMore = useCallback(() => {
+    setSeeMore((prevState) => !prevState);
   }, []);
 
   const ProjectContent = components[project];
+
+  // grab data about the project by its title
+  const projectData = (Object.values(Projects).find((value) =>
+    value.title === project ? value : null
+  ) as unknown) as ProjectContent;
+
   return (
     <div className="Project">
-      <ProjectContent img={currentImg} />
+      <Nav />
+      <ProjectContent
+        seeMore={seeMore}
+        setSeeMore={setMore}
+        is768={is768}
+        img={currentImg}
+        projectData={projectData}
+      />
       <ContactArea />
       <OtherProjects />
       <Footer />
