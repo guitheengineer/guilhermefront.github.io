15e09e733e5593ea88f6a7d969f9e0607d636487
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-24 14:41:25 -0300

    feat: update presentation styles and writing

--- src/components/sections/presentation/Presentation.tsx
+++ src/components/sections/presentation/Presentation.tsx
@@ -1,24 +1,20 @@
 import { BlueButton } from 'components/blue-button';
 import { Profile } from 'components/profile';
-import React from 'react';
+
 import './presentation.scss';
-import handsOn from 'assets/hands-on.svg';
+import notebook from 'assets/notebook.svg';
 
 const Presentation = () => (
   <div className="Presentation">
     <section className="Presentation__call">
       <h1 className="Presentation__heading">
-        <img
-          className="Presentation__present-icon"
-          alt="present-icon"
-          src={handsOn}
-        />
-        Hi, i&apos;m Guilherme
+        <img className="Presentation__present-icon" alt="" src={notebook} />I
+        build apps with
+        <span className="Presentation__strong"> awesome code quality.</span>
       </h1>
       <p className="Presentation__paragraph">
-        As a React developer, I&apos;ve been coding valuable applications with
-        continuous improvements. My goal is to always improve people daily lives
-        by providing quality software.
+        As a React developer, I have been writing code that is trustable, clean
+        and easy to maintain.
       </p>
       <a href="mailto:gsdevelopercontact@gmail.com">
         <BlueButton className="Presentation__button">Contact</BlueButton>
