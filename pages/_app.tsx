42b936cd972a6f590ee9697ad6b817a3debcb3e9
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-27 20:22:37 -0300

    feat: add global css and index nextjs components

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import 'styles/global.scss';
+import { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,40 @@
+import { Nav, ProjectsGroup, Skills, Presentation, AboutMe } from 'components';
+import styles from 'styles/home.module.scss';
+import Head from 'next/head';
+
+const Home = () => (
+  <div data-testid="Home" className={styles.home}>
+    <Head>
+      <title>Portfólio - Guilherme Samuel</title>
+      <meta name="title" content="Portfólio - Guilherme Samuel" />
+      <meta name="description" content="My projects and info about me" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://gsdeveloper.github.io/" />
+      <meta property="og:title" content="Portfólio - Guilherme Samuel" />
+      <meta property="og:description" content="My projects and info about me" />
+      <meta property="og:image" content="https://i.ibb.co/XJ0sS8r/image.png" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://gsdeveloper.github.io/" />
+      <meta property="twitter:title" content="Portfólio - Guilherme Samuel" />
+      <meta
+        property="twitter:description"
+        content="My projects and info about me"
+      />
+      <meta
+        property="twitter:image"
+        content="https://i.ibb.co/XJ0sS8r/image.png"
+      />
+    </Head>
+    <Nav />
+    <Presentation />
+    <ProjectsGroup />
+    <Skills />
+    <AboutMe />
+
+    {/*
+     
+      <Footer /> */}
+  </div>
+);
+
+export default Home;
