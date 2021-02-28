1c770a3c0c7c36b99171a9b31cc37c0948e0a59c
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-27 22:04:30 -0300

    feat: add project component

---
+++ components/project/Project.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState, useCallback } from 'react';
+import styles from './project.module.scss';
+import { OtherProjects, ContactArea, Footer, Nav } from 'components';
+import { useMediaQuery } from 'react-responsive';
+import Projects from 'projects';
+import { ProjectContent } from 'types';
+import { Quizby, BetterAim } from './project-components';
+import { useRouter } from 'next/dist/client/router';
+
+const components = {
+  quizby: Quizby,
+  betteraim: BetterAim,
+};
+
+type Projects = { project: 'betteraim' };
+
+const Project = () => {
+  const {
+    query: { project },
+  } = useRouter();
+  const [seeMore, setSeeMore] = useState(false);
+
+  const is768 = useMediaQuery({ maxWidth: 768 });
+
+  const setMore = useCallback(() => {
+    setSeeMore((prevState) => !prevState);
+  }, []);
+
+  const ProjectContent = components[project];
+
+  // grab data about the project by its title
+  const projectData = (Object.values(Projects).find((value) =>
+    value.title === project ? value : null
+  ) as unknown) as ProjectContent;
+
+  return (
+    <div className={styles.project}>
+      <Nav />
+      <ProjectContent
+        seeMore={seeMore}
+        setSeeMore={setMore}
+        is768={is768}
+        img={`assets/work-demos/${project}.png`}
+        projectData={projectData}
+      />
+      <ContactArea />
+      <OtherProjects />
+      <Footer />
+    </div>
+  );
+};
+
+export default Project;
