d3e4530f174078607cd5ebb658daf67708aec91c
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-27 22:02:17 -0300

    feat: add icon component

---
+++ components/icon/Icon.tsx
@@ -0,0 +1,44 @@
+import styles from './icon.module.scss';
+import { Tech } from 'types';
+import Image from 'next/image';
+
+export type IconProps = {
+  title: Tech;
+  type: 'work-icon' | 'profile' | 'skill';
+  className?: string;
+  size?: number;
+  imgSize?: number;
+};
+
+const Icon = ({
+  title,
+  className,
+  size = 45,
+  imgSize = 24,
+  type,
+}: IconProps) => {
+  return (
+    <div
+      style={
+        !className
+          ? {
+              width: size,
+              height: size,
+            }
+          : undefined
+      }
+      className={`${styles.icon} ${styles[`icon__${type}`]} ${
+        styles[`icon__${type}--${title}`]
+      }`}
+    >
+      <Image
+        alt={title}
+        width={imgSize}
+        height={imgSize}
+        src={`/assets/skill-icons/${title}.svg`}
+      />
+    </div>
+  );
+};
+
+export default Icon;
