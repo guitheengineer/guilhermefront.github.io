a605efba5a0d6244c80f2e5db8060d9855fd847b
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-27 22:05:48 -0300

    feat: add screen component

---
+++ components/project/Screen.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import styles from './screen.module.scss';
+
+type Props = {
+  title: string;
+  description: string;
+};
+
+const Screen = ({ title, description }: Props) => {
+  return (
+    <li className={styles.screen}>
+      <h3 className={styles.screens__title}>{title}</h3>
+      <Image
+        width={200}
+        height={350}
+        className={styles.screens__img}
+        src={`/assets/screens/${title.toLowerCase()}`}
+      />
+      <p className={styles.screens__paragraph}>{description}</p>
+    </li>
+  );
+};
+
+export default Screen;
