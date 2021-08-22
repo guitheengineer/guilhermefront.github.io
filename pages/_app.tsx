b46e2189385057a5995816df3cd9187c2aec4af1
Guilherme Samuel <devguilhermefront@gmail.com>
2021-08-22 12:27:27 -0300

    feat: add persistent layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import 'styles/global.scss';
 import { AppProps } from 'next/app';
+import { Footer, Nav } from 'components';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Nav />
+    <main className="container">
+      <Component {...pageProps} />
+    </main>
+    <Footer />
+  </>
+);
 
-export default MyApp;
+export default App;
