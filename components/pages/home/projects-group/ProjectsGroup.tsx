f067ebe70876343e82b4ffe1d5173cd2d70bc6c9
Guilherme Samuel <devguilhermefront@gmail.com>
2021-11-27 22:09:00 -0300

    refactor: change projectsgroup to use tailwind

--- components/pages/home/projects-group/ProjectsGroup.tsx
+++ components/pages/home/projects-group/ProjectsGroup.tsx
@@ -1,14 +1,13 @@
 import { Work } from 'components';
 import { Heading } from 'components';
 import { Projects } from 'projects';
-import styles from './projects-group.module.scss';
 
 type Props = {
   quantity?: number;
 };
 
 export const ProjectList = ({ quantity }: Props) => (
-  <div className={styles['projects-group__works']}>
+  <div className="grid lg:grid-cols-2 gap-8 mt-6">
     {Projects.slice(0, quantity ? quantity : Projects.length).map((project) => (
       <Work key={project.title} {...project} />
     ))}
@@ -17,7 +16,7 @@ export const ProjectList = ({ quantity }: Props) => (
 
 export const ProjectsGroup = () => {
   return (
-    <article className={styles['projects-group']}>
+    <article className="mt-24 md:mt-32">
       <Heading>Projects</Heading>
       <ProjectList quantity={6} />
     </article>
